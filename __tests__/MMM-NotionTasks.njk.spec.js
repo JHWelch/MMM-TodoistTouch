10a8ac7dc995602bb08e3e0823bc405fa38ebbf8
Jordan Welch <jordanhwelch@gmail.com>
2026-08-17 16:13:01 -0500

    Add task display

--- __tests__/MMM-NotionTasks.njk.spec.js
+++ __tests__/MMM-NotionTasks.njk.spec.js
@@ -15,3 +15,21 @@ describe('loading', () => {
     expect(template).toContain('LOADING');
   });
 });
+
+describe('loaded with tasks', () => {
+  beforeEach(() => {
+    data = {
+      loading: false,
+      tasks: [
+        { content: 'Task 1' },
+        { content: 'Task 2' },
+      ],
+    };
+    template = nunjucks.render('MMM-TodoistTouch.njk', data);
+  });
+
+  it('shows tasks', () => {
+    expect(template).toContain('Task 1');
+    expect(template).toContain('Task 2');
+  });
+});
